abc5a48770cb34f0a5968ccde68048e71b22bfd7
Dalton Overmyer <overmyerms13dalton@gmail.com>
2022-09-01 22:35:19 -0500

    removed comment

--- main.c
+++ main.c
@@ -9,26 +9,3 @@ int main(int argc, char** argv) {
   fatal("hello %s: %d", "Dalton", 101);
   return 0;
 }
-
-// #include <stdio.h>
-// #include <time.h>
-
-// #define MAX_SIZE 24
-// #define FORMAT   "%Y-%B-%d"
-
-// #define test(fmt, args...) \
-//   char buffer[MAX_SIZE]; \
-//   time_t t = time(NULL); \
-//   strftime(buffer, MAX_SIZE, FORMAT, localtime(&t)); \
-//   printf(fmt " [%s]\n", ##args, buffer)
-
-// int main() {
-//   test("test: %d", 101);
-
-//   // char buffer[MAX_SIZE];
-//   // time_t t = time(NULL);
-//   // strftime(buffer, MAX_SIZE, FORMAT, localtime(&t));
-//   // printf("%s\n", buffer);
-
-//   return (0);
-// }
