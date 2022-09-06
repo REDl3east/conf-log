82798866146eb51d18fe406d0d5687678888a2a9
Dalton Overmyer <overmyerms13dalton@gmail.com>
2022-09-05 23:17:12 -0500

    updated example main function

--- examples/example2/main.c
+++ examples/example2/main.c
@@ -1,11 +1,11 @@
 #include "conf-log-conf.h"
 
 int main(int argc, char** argv) {
-  trace("hello %s: %d", "Thomas", 62);
-  debug("hello %s: %d", "Dalton", 26);
-  info("hello %s: %d", "Dalton", 26);
-  warn("hello %s: %d", "Dalton", 26);
-  error("hello %s: %d", "Dalton", 26);
-  fatal("hello %s: %d", "Dalton", 101);
+  trace("This is a %s log: %d", "trace", 1);
+  debug("This is a %s log: %d", "debug", 2);
+  info("This is a %s log:  %d", "info", 3);
+  warn("This is a %s log:  %d", "warn", 4);
+  error("This is a %s log: %d", "error", 5);
+  fatal("This is a %s log: %d", "fatal", 6);
   return 0;
 }
